ae7f2e09591db4b0978c976d4a54a578457c41c5
RK905 <mr.rayenk@gmail.com>
2015-09-23 13:33:39 -0400

    Model & Location Manager
    Part 1

---
+++ Pods/Target Support Files/Pods-SwiftChaseBranch/Pods-SwiftChaseBranch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftChaseBranch : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftChaseBranch
+@end
---
+++ SwiftChaseBranch/ChaseATM.swift
@@ -0,0 +1,43 @@
+//
+//  ChaseATM.swift
+//  SwiftChaseBranch
+//
+//  Created by Rayen Kamta on 9/23/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class ChaseATM{
+    
+    private var _name:String!
+    private var _address:String!
+    private var _distance:String!
+    private var _locType:String!
+   
+    
+    
+    
+    
+    
+    var name :String{
+        return _name
+    }
+    var address :String{
+        return _address
+    }
+    var distance :String{
+        return _distance
+    }
+    var locType :String{
+        return _locType
+    }
+    
+
+    
+    
+    
+    
+}
